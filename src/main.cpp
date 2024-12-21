adc922b1ae413cedee46248845c3691ab6edb5fd
Hong635 <hongkng5@uw.edu>
2024-12-20 20:54:24 -0800

    update config and define constants

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pico/stdio_usb.h>
+#include "main.h"
 
-//FreeRTOS
+// FreeRTOS
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 
-
-
+// handle for each task
 TaskHandle_t usbInTaskHandle;
 TaskHandle_t usbOutTaskHandle;
 
+TaskHandle_t usbTaskHandle;
+TaskHandle_t lightTaskHandle;
+TaskHandle_t stepperMotorTaskHandle;
+TaskHandle_t pwmServoTaskHandle;
+
 QueueHandle_t dataToHostQueue;
 
+// example task
 void exampleTask(void *param)
 {
     while (!stdio_usb_connected())
@@ -23,24 +29,57 @@ void exampleTask(void *param)
     }
     printf("stdio_usb_connected()\n");
 
-    while (true) {
+    while (true)
+    {
         printf("hello from task\n");
         sleep_ms(1000);
     }
-    
 }
 
+// usb task
+void usbTask(void *params)
+{
+    // TODO: add definition
+}
 
-void tenSecDebugLED(){
+// light task
+void lightTask(void *params)
+{
+    // TODO: add definition
+}
+
+// stepper motor task
+void stepperMotorTask(void *params)
+{
+    // TODO: add definition
+}
+
+// servo task
+void pwmServoTask(void *params)
+{
+    // TODO: add definition
+}
+
+// for sending i2c cmd
+void i2cSetPin(uint8_t expanderAddress, uint8_t pinNumber)
+{
+    // TODO: add definition
+}
+
+// example debug function
+void tenSecDebugLED()
+{
     // Blink 5 times in 10 seconds
-    for (int index = 0; index < 10; index++) {
-        gpio_put(25,index % 2);
+    for (int index = 0; index < 10; index++)
+    {
+        gpio_put(25, index % 2);
         sleep_ms(1000);
         printf("%d \n", index);
     }
 }
 
-void onBootInit(){
+void onBootInit()
+{
     stdio_init_all();
 
     // This is the LED
@@ -48,14 +87,48 @@ void onBootInit(){
     gpio_set_dir(25, GPIO_OUT);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     onBootInit();
 
-    #if (DEBUG)
-        tenSecDebugLED();
-    #endif
+#if (DEBUG)
+    tenSecDebugLED();
+#endif
 
+    // example task
+    // note that the stack size is in words, NOT bytes
+    // (see doc for explanation)
     xTaskCreate(exampleTask, "USB_DATA_IN", configMINIMAL_STACK_SIZE * 2, NULL, 1, &usbInTaskHandle);
+
+    // all default stack size, change if needed
+    // min stack size is 128 words
+    xTaskCreate(usbTask,                  // task func name
+                "USB_TASK",               // name in char, max 16 chars
+                configMINIMAL_STACK_SIZE, // stack depth
+                NULL,                     // params
+                USB_TASK_PRIORITY,        // priority
+                &usbOutTaskHandle);       // handle
+
+    xTaskCreate(lightTask,
+                "LIGHT_TASK",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                LIGHT_TASK_PRIORITY,
+                &lightTaskHandle);
+
+    xTaskCreate(stepperMotorTask,
+                "STEPPER_TASK",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                STEPPER_TASK_PRIORITY,
+                &stepperMotorTaskHandle);
+
+    xTaskCreate(pwmServoTask,
+                "SERVO_TASK",
+                configMINIMAL_STACK_SIZE,
+                NULL,
+                PWM_SERVO_TASK_PRIOTITY,
+                &pwmServoTaskHandle);
 
     vTaskStartScheduler();
 
